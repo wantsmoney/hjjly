b10602cb73644127f8f5600f88bb5985d4f8df9f
15775100205 <2658624008@qq.com>
2023-06-12 16:57:25 +0800

    test 3

--- src/http/axios.js
+++ src/http/axios.js
@@ -43,3 +43,19 @@ promise.then((res)=>{
 }).catch(err=>{
     
 })
+
+
+
+const service = axios.create({
+
+    baseURL:
+  
+     process.env.NODE_ENV === 'dev'
+  
+        ? process.env.VUE_APP_DEV_API
+  
+        : process.env.VUE_APP_PRODUCTION_API,
+  
+    timeout: 10000
+  
+  })
